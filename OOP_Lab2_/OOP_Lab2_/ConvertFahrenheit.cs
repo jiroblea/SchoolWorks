f05e6c1377de5b4ce0c2a70abeb6dfedaacee0ef
jiroblea <jirooblea@gmail.com>
2021-08-02 17:57:19 +0800

    added OOP_Lab2_ project containing codes for lab2

---
+++ OOP_Lab2_/OOP_Lab2_/ConvertFahrenheit.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace OOP_Lab2_
+{
+    class ConvertFahrenheit
+    {
+        public double temperature;
+
+        private double CalculateCelsius()
+        {
+            double celsius = (temperature - 32) * 5 / 9;
+            return Math.Round(celsius, 2);
+        }
+
+        public void DisplayResults()
+        {
+            Console.WriteLine("Original Value\t: " + temperature + " Fahrenheit");
+            Console.WriteLine("Converted Value\t: " + CalculateCelsius() + " Celsius");
+        }
+    }
+}
---
+++ OOP_Lab2_/OOP_Lab2_/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace OOP_Lab2_
+{
+    class Program
+    {
+        public static double GetTemperature()
+        {
+            Console.WriteLine("Enter temperature in Fahrenheit:");
+            string input = Console.ReadLine();
+            double temperature = Convert.ToDouble(input);
+            return temperature;
+        }
+
+        static void Main(string[] args)
+        {
+            DateTime timeStamp = DateTime.Now;
+
+            ConvertFahrenheit fahrenheit1 = new ConvertFahrenheit();
+
+            fahrenheit1.temperature = GetTemperature();
+            fahrenheit1.DisplayResults();
+
+            Console.WriteLine("Time Stamp\t: " + timeStamp.ToString("F"));
+        }
+    }
+}
