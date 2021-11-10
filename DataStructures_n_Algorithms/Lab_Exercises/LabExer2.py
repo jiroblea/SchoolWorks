0bf86a9696babade78fe3c664ce3b9aea254d2ea
jiroblea <jirooblea@gmail.com>
2021-11-10 23:53:54 +0800

    added function to display outputs properly and shorten the code in problem 1 and started working on problem 2-its functions and if else statements

--- DataStructures_n_Algorithms/Lab_Exercises/LabExer2.py
+++ DataStructures_n_Algorithms/Lab_Exercises/LabExer2.py
@@ -3,14 +3,15 @@
 # Subject: CMPE 30052 Data Structures and Algorithms 
 # Laboratory Exercise No. 1
 
+import math
+
 # Problem 1
 # Create a program that will add, subtract, multiply and divide two numbers.
 # - Use user defined function for each operator
 # - Any number divided by zero will result to undefined
 # - Use int data type only for all variables declared
 
-
-def basic_math_operation():
+def basic_math_operations():
     def addition(num1: int, num2: int) -> int:
         return num1 + num2
 
@@ -21,9 +22,8 @@ def basic_math_operation():
         return num1 * num2
 
     def division(num1: int, num2: int): 
-        """TODO: should not return float if possible"""
         try:
-            return num1 // num2
+            return round(num1 / num2, 2)
         except ZeroDivisionError:
             return "undefined!"
 
@@ -34,6 +34,16 @@ def basic_math_operation():
             return num1, num2
         except ValueError:
             print("The program only accept integers.")
+
+    def display_output(result_name: str, fnctn) -> None:
+        try:
+            num = ask_number_input()
+            if fnctn == division and num[1] == 0:
+                print("Undefined!")
+            else:
+                print(f"The {result_name} is", fnctn(num[0], num[1]))
+        except:
+            print("Try again.")
 
     while True:
         print("\n***********************\nMENU\n***********************")
@@ -49,45 +59,76 @@ def basic_math_operation():
             print("Thank you!")
             input("Press any key to continue...")
             break
-
         elif choice == "a":
             print("\nADDITION")
-            try:
-                num = ask_number_input()
-                print("The sum is", addition(num[0], num[1]))
-            except:
-                print("Try again.")
-
+            display_output("sum", addition)
         elif choice == "s":
             print("\nSUBTRACTION")
-            try:
-                num = ask_number_input()
-                print("The difference is", subtraction(num[0], num[1]))
-            except:
-                print("Try again.")
-
+            display_output("difference", subtraction)
         elif choice == "m":
             print("\nMULTIPLICATION")
-            try:
-                num = ask_number_input()
-                print("The product is", multiplication(num[0], num[1]))
-            except:
-                print("Try again.")
-                
+            display_output("product", multiplication)
         elif choice == "d":
             print("\nDIVISION")
-            try:
-                num = ask_number_input()
-                print("The quotient is", division(num[0], num[1]))
-            except:
-                print("Try again.")
+            display_output("quotient", division)
         else:
+            print("Choose only from the choices.")
+
+
+# Problem 2
+# Create a program compute the area of the following polygons and circle.
+# - Area of square given the side.
+# - Area of rectangle given the length and width.
+# - Area of triangle given the base and height.
+# - Area of circle given the radius
+
+def compute_area():
+    def area_of_square(side: float) -> float:
+        return round(side * side, 2)
+
+    def area_of_rectangle(length: float, width: float) -> float:
+        return round(length * width, 2)
+    
+    def area_of_triangle(base: float, height: float) -> float:
+        return round((base * height) / 2, 2)
+
+    def area_of_circle(radius: float) -> float:
+        return round(math.pi * (radius ** 2), 2)
+
+    while True:
+        print("\n***********************\nMENU\n***********************")
+        print("[1] - Area of square") 
+        print("[2] - Area of rectangle")
+        print("[3] - Area of triangle")
+        print("[4] - Area of circle")
+        print("[5] - exit")
+        print("-----------------------")
+
+        choice = input("Enter your choice: ")
+        print("-----------------------")
+
+        if choice == "5":
+            print("Thank you!")
+            input("Press any key to continue...")
+            break
+        elif choice == "1":
+            print("AREA OF SQUARE")
+            print("-----------------------")
+            side = float(input("Enter the side of the square: "))
+            print(f"\nThe area is {area_of_square(side)} sq. units")
+        elif choice == "2":
+            print("AREA OF RECTANGLE")
+            print("-----------------------")
+            len_wid = input("Enter the length and the width of the rectangle: ").split(" ")
+            print(f"\nThe area is {area_of_rectangle(float(len_wid[0]), float(len_wid[1]))} sq. units")
+        else: 
             print("Choose only from the choices.")
 
 
 def main():
     print("Hello World!")
-    basic_math_operation()
+    basic_math_operations()
+    compute_area()
 
 
 if __name__ == "__main__":
