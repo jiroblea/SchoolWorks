86bdd8c841c672e2f348cec60fb63b589fa56704
jiroblea <jirooblea@gmail.com>
2022-01-17 22:47:35 +0800

    added LabExer5 problem statements

---
+++ DataStructures_n_Algorithms/Lab_Exercises/LabExer5.py
@@ -0,0 +1,30 @@
+# Student: Junius Iosif R. Oblea
+# Program: BS Computer Engineering
+# Subject: CMPE 30052 Data Structures and Algorithms
+# Laboratory Exercise No. 5
+
+
+# Problem 1
+# Create a program that will ask the user to enter the student record with three quizzes. The program will
+# display the student record together with the average grade of the three quizzes and display a remark if
+# the student passed the subject or not. The structure must compose of three members the id, the name,
+# and the quiz in array form.
+
+
+# Problem 2
+# Create a program that will enter 5 student records with three quizzes. The program will generate an
+# output that will displays all the student records with their grades and remarks.
+# grades = average grade of the three quizzes
+# 75 is the passing mark.
+
+
+# Problem 3
+# Create a program that will give an output as shown on figure A.The program will ask to input three
+# customers information. Each customer have three items purchased. 
+
+
+def main():
+    pass
+
+if __name__ == "__main__":
+    main()
