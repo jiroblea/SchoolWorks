f2773ee08179993746c1e0a8b294324b3b976164
jiroblea <jirooblea@gmail.com>
2022-01-23 18:39:14 +0800

    created the solution for LabExer5 problem 2

--- DataStructures_n_Algorithms/Lab_Exercises/LabExer5.py
+++ DataStructures_n_Algorithms/Lab_Exercises/LabExer5.py
@@ -3,6 +3,8 @@
 # Subject: CMPE 30052 Data Structures and Algorithms
 # Laboratory Exercise No. 5
 
+import re
+from tabulate import tabulate
 
 def try_again():
     """
@@ -41,7 +43,7 @@ def student_record():
                 student_quiz3 = float(input("Quiz 3: "))
                 break
             except ValueError:
-                print("Only accepts numbers.")
+                print("Error: Please enter a number only.")
 
         return student_id, student_name, student_quiz1, student_quiz2, student_quiz3
 
@@ -69,11 +71,13 @@ def student_record():
         print(f"Name: {student_name}")
         print(f"Grades: {average_grade}")
         print(f"Remarks: {remarks}")
-    
+        return None
+        
 
     var_student_record = ask_student_record()
     var_average_grade = quiz_average_grade(var_student_record[2], var_student_record[3], var_student_record[4])
     display_student_record(var_student_record[0], var_student_record[1], var_average_grade)
+    return None
 
 
 
@@ -82,6 +86,81 @@ def student_record():
 # output that will displays all the student records with their grades and remarks.
 # grades = average grade of the three quizzes
 # 75 is the passing mark.
+
+def five_student_record():
+    """
+    Program for problem 2
+    """
+
+    def quiz_average_grade(student_quiz1, student_quiz2, student_quiz3):
+        """
+        Evaluates the average of 3 quizzes.
+        """
+        average_grade = (student_quiz1 + student_quiz2 + student_quiz3) / 3
+        return float("{:.2f}".format(average_grade))
+
+    
+    def ask_5_student_record():
+        """
+        Asks the user to enter five student records with three quizzes. 
+        """
+
+        # lists of student record
+        student_id_list = []
+        student_name_list = []
+        student_grade_list = []
+
+        print("Enter 5 Student(s) Record")
+
+        for i in range(1,6):
+            print(f"\nStudent {i}")
+            student_id = input("ID: ")
+            student_name = input("Name: ")
+            student_3_quizzes_float = []
+            
+            # asks and checks if the 3 quizzes scores are numbers
+            while True:
+                try:
+                    student_3_quizzes_str = input("Enter 3 quiz(zes): ").split()
+                    for i in student_3_quizzes_str:
+                        student_3_quizzes_float.append(float(i))
+                    break
+                except ValueError:
+                    print("Error: Please enter a number only.")
+        
+            student_grade = quiz_average_grade(student_3_quizzes_float[0], student_3_quizzes_float[1], student_3_quizzes_float[2])
+
+            student_id_list.append(student_id)
+            student_name_list.append(student_name)
+            student_grade_list.append(student_grade)
+
+        return student_id_list, student_name_list, student_grade_list
+
+
+    def display_5_student_record(student_id_list, student_name_list, student_grade_list):
+        
+        student_remark_list = []
+        for grade in student_grade_list:
+            if grade >= 75:
+                student_remark_list.append("Passed")
+            else: 
+                student_remark_list.append("Failed")
+
+        record = {"No.": [1, 2, 3, 4, 5],
+                "Student No": student_id_list,
+                "Name": student_name_list, 
+                "Grade": student_grade_list, 
+                "Remark": student_remark_list}
+
+        print(" ")
+        print(tabulate(record, headers="keys", tablefmt="plain", stralign="right"))
+        return None
+
+
+    five_student_record = ask_5_student_record()
+    display_5_student_record(five_student_record[0], five_student_record[1], five_student_record[2])
+    return None
+
 
 
 # Problem 3
@@ -95,6 +174,9 @@ def main():
         student_record()
         if try_again() is False:
             break
+
+    # Problem 2
+    five_student_record()
 
 if __name__ == "__main__":
     main()
